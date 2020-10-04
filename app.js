d84bca7cc6ee39a7a90f6086df9a6e2ec8596e63
letsgitthis <jbentley1011@gmail.com>
2020-10-04 00:47:30 -0500

    added sendstatus(403)

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+// GET api
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API'
     });
 });
 
-app.post('/api/posts', (req, res) => {
+// POST api for a post
+app.post('/api/posts', verifyToken, (req, res) => {
     res.json({
         message: 'Post created...'
     });
 });
 
+// POST api for user login
 app.post('/api/login', (req, res) => {
     // user
     const user = {
@@ -23,11 +26,29 @@ app.post('/api/login', (req, res) => {
         email: 'example@email.com'
     }
     
+    // payload {user: 'John'}
     jwt.sign({user}, 'secret key', (err, token) => {
         res.json({
             token
         });
     });
 });
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// VERIFY TOKEN 
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+        
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+
+}
 
 app.listen(5000, () => console.log('Server started on port 5000'));
